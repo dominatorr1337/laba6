628410363eb2c2c9ba501c3dbd569e44edc2c254
dominatorr1337 <dominatorr1337@gmail.com>
2026-03-28 16:18:25 +0300

    Update Garden.h

--- Garden.h
+++ Garden.h
@@ -4,65 +4,82 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <vector>
-#include <memory>
-#include <algorithm>
-#include <sstream>
+#include <cstring>
 #include <limits>
-
-using namespace std;
 
 class Plant {
 private:
     int id;
-    string name;
-    string type;
+    std::string name;
+    std::string type;
     int growth_time_months;
+    Plant* next;
 
 public:
     Plant();
-    Plant(int id, const string& name, const string& type, int growth_time);
+    Plant(int id, const std::string& name, const std::string& type, int growth_time);
+    ~Plant();
+    
+    Plant(const Plant& other);
+    Plant& operator=(const Plant& other);
     
     int getId() const;
-    string getName() const;
-    string getType() const;
+    std::string getName() const;
+    std::string getType() const;
     int getGrowthTime() const;
     
-    void setName(const string& new_name);
-    void setType(const string& new_type);
+    void setName(const std::string& new_name);
+    void setType(const std::string& new_type);
     void setGrowthTime(int new_time);
     
-    void display() const;
-    string toString() const;
+    Plant* getNext() const;
+    void setNext(Plant* next);
     
-    static Plant fromString(const string& line);
+    void display() const;
+    std::string toString() const;
+    
+    static Plant fromString(const std::string& line);
+    
+    friend class GardenDB;
 };
 
 class GardenDB {
 private:
-    vector<Plant> plants;
+    Plant* head;
+    Plant* tail;
+    int count;
     int next_id;
+    std::string filename;
     
     Plant* findPlantById(int id);
     const Plant* findPlantById(int id) const;
     
+    void clear();
+
 public:
     GardenDB();
+    explicit GardenDB(const std::string& filename);
+    ~GardenDB();
+    
+    GardenDB(const GardenDB& other) = delete;
+    GardenDB& operator=(const GardenDB& other) = delete;
     
     void addPlant();
     bool deletePlant(int id);
     bool editPlant(int id);
     void displayAllPlants() const;
     
-    const Plant* findPlantByName(const string& name) const;
-    const Plant* findPlantByType(const string& type) const;
-    vector<const Plant*> findAllPlantsByType(const string& type) const;
+    const Plant& findPlantByName(const std::string& name) const;
+    const Plant& findPlantByType(const std::string& type) const;
     
-    bool saveToFile(const string& filename) const;
-    bool loadFromFile(const string& filename);
+    bool saveToFile() const;
+    bool loadFromFile();
     
     int getPlantCount() const;
     bool isEmpty() const;
+    
+    void setFilename(const std::string& filename);
+    std::string getFilename() const;
     
     static void showMenu();
     static int getChoice();
